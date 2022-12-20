8a3ac928130b0676faca9b1e7a04978ea00f7117
clym <jcignorance@outlook.com>
2022-12-20 23:02:27 +0800

    feat: add analytics (#6)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { render } from 'solid-js/web'
 import { Router } from '@solidjs/router'
+import { inject } from '@vercel/analytics'
 import '@/assets/styles/global.less'
 import { App } from './App'
+
+inject({
+  mode: import.meta.env.PROD ? 'production' : 'development'
+})
 
 render(() => (
   <Router>
