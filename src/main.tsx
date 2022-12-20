afc6500d922cef40488449f8be8a61c52ec4e9d3
cl <jcignorance@outlook.com>
2022-12-20 23:19:20 +0800

    fix: vercel analytics'

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { render } from 'solid-js/web'
 import { Router } from '@solidjs/router'
-import { inject } from '@vercel/analytics'
 import '@/assets/styles/global.less'
 import { App } from './App'
-
-inject({
-  mode: import.meta.env.PROD ? 'production' : 'development'
-})
 
 render(() => (
   <Router>
