f005b3614562d78b94d32f831e05cc21997da512
cl <jcignorance@outlook.com>
2023-03-12 22:14:24 +0800

    chore: add stack

--- src/pages/resume/components/stack/index.tsx
+++ src/pages/resume/components/stack/index.tsx
@@ -5,7 +5,8 @@ export const Stack = () => {
         <li>熟练使用vue3+typescript+less开发大型应用</li>
         <li>对于系统设计，API设计，大型项目管理有自己的认识</li>
         <li>熟练使用git</li>
-        <li>推崇code review, 追求代码质量</li>
+        <li>推崇CR, 追求代码质量</li>
+        <li>熟悉react、solid、node等技术，ES6+等语法</li>
       </ul>
     </div>
   )
